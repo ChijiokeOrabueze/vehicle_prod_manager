a6798d56898ed87261c09c76aeeaaf51b17a5e4d
Chijioke Orabueze <valentineorabueze@gmail.com>
2023-09-08 07:55:28 +0100

    update: implemented standard rensponse handler; used standardized response for state routes

--- app-api/app/controllers/api/v1/states_controller.rb
+++ app-api/app/controllers/api/v1/states_controller.rb
@@ -6,18 +6,18 @@ module Api
     class StatesController < ApplicationController
       before_action :set_state, only: %i[ show update destroy ]
       
-      @@handler = ResponseHandler.new
+      # @@handler = ResponseHandler.new
       # GET /states
       def index
         @states = State.all
 
-        @@handler.handle_response({data: @states, success: true}, "fetched all states.")
-        # render json: @states
+        handle_response({"data" => @states, "success" => true}, "fetched all states.")
       end
 
       # GET /states/1
       def show
-        render json: @state
+        handle_response({"data" => @states, "success" => true}, "fetched state.")
+        
       end
 
       # POST /states
@@ -25,30 +25,50 @@ module Api
         @state = State.new(state_params)
 
         if @state.save
-          render json: @state, status: :created, location: @state
+          handle_response({"data" => @state, "success" => true}, "state created successfully.")
         else
-          render json: @state.errors, status: :unprocessable_entity
+          handle_response({
+              "success" => false,
+              "message" => "Requested failed.", 
+              "errors" => @state.errors
+          }, "", 413)
         end
       end
 
       # PATCH/PUT /states/1
       def update
         if @state.update(state_params)
-          render json: @state
+          handle_response({"data" => @state, "success" => true}, "state updated successfully.")
         else
-          render json: @state.errors, status: :unprocessable_entity
+          handle_response({
+              "success" => false,
+              "message" => "Requested failed.", 
+              "errors" => @state.errors
+          }, "", 413)
         end
       end
 
       # DELETE /states/1
       def destroy
         @state.destroy
+        handle_response({"data" => @state, "success" => true}, "state deleted successfully.")
       end
 
       private
         # Use callbacks to share common setup or constraints between actions.
         def set_state
-          @state = State.find(params[:id])
+          begin
+        
+            @state = State.find(params[:id])
+          
+          rescue ActiveRecord::RecordNotFound
+            handle_response({
+                  "success" => false,
+                  "message" => "Requested state not found.", 
+                  "errors" => "Requested state not found."
+              })
+          end
+          
         end
 
         # Only allow a list of trusted parameters through.
--- app-api/app/controllers/application_controller.rb
+++ app-api/app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+
+
+
+    def handle_response(response, message="", statusCode=400)
+
+        status = {
+            400 => :bad_request,
+            413 => :unprocessable_entity,
+            500 => :internal_server_error,
+        }
+        
+        if response["success"]
+            render json: {status: "success", message: message, data: response["data"]}, status: :ok
+        elsif response["internal_error"]
+            render json: {status: "error", message: "Something went wrong. Please try again later.", errors: "Something went wrong. Please try again later."},status: :internal_server_error
+        else
+            render json: {status: "failed", message: response["message"], errors: response["errors"]}, status: status[statusCode] || :internal_server_error
+        end
+    end
+
 end
